34aa8533a54b27a108d2d4178ce6aae7edc1890e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-02 15:39:50 +0900

    add

--- test/index.test.js
+++ test/index.test.js
@@ -2,4 +2,12 @@
 const assert = require("assert")
 const { searchIndex, searchData } = require("../src/index")
 
-console.log(searchIndex("lie"))
+// searchIndex
+const result = searchIndex("lie")
+assert.deepStrictEqual(result.verb.offsets,[
+  2696550, 1549783,
+  2659476, 2740213,
+    835938, 2737265,
+  1989043
+])
+assert.deepStrictEqual(result.noun.offsets, [ 6769542, 11151798, 8641771 ])
