7afb73f642a5a4cd4b863dc2f60e7270bfb3ac8d
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-02 15:41:04 +0900

    add

--- test/index.test.js
+++ test/index.test.js
@@ -10,4 +10,17 @@ assert.deepStrictEqual(result.verb.offsets,[
     835938, 2737265,
   1989043
 ])
-assert.deepStrictEqual(result.noun.offsets, [ 6769542, 11151798, 8641771 ])
+assert.deepStrictEqual(result.noun.offsets, [6769542, 11151798, 8641771])
+
+
+const offset = searchData(2659476)
+
+assert.deepStrictEqual(
+  offset.words, ['dwell', 'consist', 'lie', 'lie_in']
+)
+assert.deepStrictEqual(
+  offset.glossary,[
+    'originate (in)',
+    '"The problems dwell in the social injustices in this country"'
+  ]
+)
