a8e6f9860b2021b080246992cdc6dab08abde006
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-03 22:42:32 +0900

    add

--- generator/2-generate.js
+++ generator/2-generate.js
@@ -8,17 +8,35 @@ const wordToDigest = (l) => {
   return md5(l).slice(0, 2)
 }
 
+const mapToObj = (mapObj) => Object.fromEntries([...mapObj.entries()])
+
 const itemsToDigests = (obj) => {
   const digests = {}
-  Object.entries(obj).map( ([key, value]) => {
-    const dig = wordToDigest(key)
-    const map = digests[dig] ?? {}
-    digests[dig] = {
-       ...map,
-       [key]: value
+  const ent = Object.entries(obj)
+  console.log(ent.length)
+  ent.map(([key, value], i) => {
+    if (i % 10000 === 0) {
+      console.log(i)
     }
+    const dig = wordToDigest(key)
+    // const map = digests[dig] ?? {}
+    // digests[dig] = {
+    //    ...map,
+    //    [key]: value
+    // }
+    digests[dig] = digests[dig] ?? new Map()
+    digests[dig].set(key, value)
+    // // digests[dig] = {
+    // //    ...map,
+    // //    [key]: value
+    // // }
   })
-  return digests
+  return Object.fromEntries(
+    Object.entries(digests).map(([k, v]) => {
+      return [k, Object.fromEntries(v)]
+    })
+  )
+  // return mapToObj(digests)
 }
 
 const saveDigests = (dir, digests) => {
@@ -61,6 +79,11 @@ const splitDigest = (items,dir) => {
     return [item.id, item]
   })
   const digs = itemsToDigests(Object.fromEntries(entries))
+  saveDigests(dir, digs)
+}
+const splitDigestObj = (obj,dir) => {
+  const digs = itemsToDigests(obj)
+  // console.log(Object.values(digs).map(d => d.length))
   saveDigests(dir, digs)
 }
 
@@ -143,43 +166,84 @@ const splitDigest = (items,dir) => {
 //   // generateSynsetSource(obj.LexicalResource.Lexicon.Synset)
 // }
 
-const generateLexIndex = () => {
-  const lex = require("../source/lex")
-  const lemmaIdx = {}
-  const synsetIdx = {}
-  lex.slice(0, 10).map(l => {
-    console.log(JSON.stringify(l, null, 2))
-    
-    // lemma
-    const lemma = l.lemma.writtenForm 
-    const newL = lemmaIdx[lemma] ?? []
-    lemmaIdx[lemma] = [...newL, l.id]
+const writeMap = (path, map) => {
+  fs.writeFileSync(path,
+    JSON.stringify(Object.fromEntries(
+      map.entries()
+    ), null, 2)
+  )
 
-    // synset
-    const synsets = l.sense.map(s => s.synset)
-    
-  })
-  // const 
 }
 
-const generateDataFiles = () => {
+const generateLemmaIndex = (lex) => {
+  const lemmaIdx = new Map()
+  lex.map(l => {
+    // lemma
+    const lemma = l.lemma.writtenForm 
+    lemmaIdx.set(lemma, [
+      ...(lemmaIdx.get(lemma) ?? []),
+      l.id
+    ])
+  })
+  return lemmaIdx
+}
+
+const generateSynsetIndex = (lex) => {
+  const synsetIdx = new Map()
+  lex.map((l) => {
+    // synset
+    l.sense.map( ({synset}) => {
+      const befS = synsetIdx.get(synset) ?? []
+      synsetIdx.set(synset, [...befS, l.id])
+    })
+  })
+  return synsetIdx
+}
+
+const generateLexMap = (lex) => {
+  const lexMap = new Map()
+  lex.map((l) => {
+    lexMap.set(l.id, l)
+  })
+  return lexMap
+}
+
+
+const structLexs = (lexMap, lemmaMap) => {
+  const lemmas = [...lemmaMap.entries()]
+  return Object.fromEntries(lemmas.map(([k, v]) => {
+    const vs = v.map(vv => [vv, lexMap.get(vv)])
+    return [k, Object.fromEntries(vs)]
+  }))
+}
+const structSyn = (lexMap, synsetArr) => {
+
+}
+
+const generateDataFiles = (lex,syn) => {
   console.log("start")
+  
+  console.log("struct map")
+  const lexMap = generateLexMap(lex)
+  const lemmaMap = generateLemmaIndex(lex)
+  const synsetMap = generateSynsetIndex(lex)
 
   console.log("lex")
-  const lex = require("../source/lex")
-  splitDigest(lex, "dic/lex")
-  // generateLexicalEntries(lex)
+  const l = structLexs(lexMap, lemmaMap)
+  console.log("lex split")
+  splitDigestObj(l, "dic/lex")
   
   console.log("syn")
-  const syn = require("../source/syn")
-  splitDigest(syn, "dic/syn")
+  // splitDigest(syn, "dic/syn")
   // generateSynset(syn)
-
+  
   console.log("3")
 }
 const start = () => {
-  // generateDataFiles()
-  generateLexIndex()
+  const lex = require("../source/lex")
+  const syn = require("../source/syn")
+  generateDataFiles(lex,syn)
+  // generateLexIndex()
 }
 
 start()
