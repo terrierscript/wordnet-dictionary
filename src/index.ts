70870c1cfaeb8a88de65ff853ffc3890b23a9a75
terrierscript <13282103+terrierscript@users.noreply.github.com>
2022-01-08 23:48:32 +0900

    add

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import md5 from "md5"
-import { LexicalEntry } from "./scheme"
+import { Lemma } from "./types/Lemma"
+import { LexicalEntry } from "./types/LexicalEntry"
+import { Sense } from "./types/Sense"
+import { SenseIndex } from "./types/SenseIndex"
+import { Synset } from "./types/Synset"
+import { SynsetIndex } from "./types/SynsetIndex"
 
 const wordToDigest = (l: string) => {
   return md5(l).slice(0, 2)
@@ -17,12 +22,12 @@ const getItem = <T>(dirname: string, key: string): T | undefined => {
 
 
 export const getLexicalEntry = (id: string) => getItem<LexicalEntry>("lex", id)
-export const getSense = (id: string) => getItem("sense", id)
-export const getSynset = (id: string) => getItem("syn", id)
-export const getLemma = (lemma: string) => getItem("lemma", lemma)
-export const getSenseIndex = (id: string) => getItem("senseidx", id)
-export const getSynsetIndex = (synsetId: string) => getItem("synidx", synsetId)
-export const getSyntacticBehaviour = (senseId: string) => getItem("behavier", senseId)
+export const getSense = (id: string) => getItem<Sense>("sense", id)
+export const getSynset = (id: string) => getItem<Synset>("syn", id)
+export const getLemma = (lemma: string) => getItem<Lemma>("lemma", lemma)
+export const getSenseIndex = (id: string) => getItem<SenseIndex>("senseidx", id)
+export const getSynsetIndex = (synsetId: string) => getItem<SynsetIndex>("synidx", synsetId)
+export const getSyntacticBehaviour = (senseId: string) => getItem<Behavior>("behavier", senseId)
 export const getRandomWord = () => {
   const dig = `${(Math.floor(Math.random() * 16)).toString(16)}${(Math.floor(Math.random() * 16)).toString(16)}`
   const json = getFile("lemma", dig)
