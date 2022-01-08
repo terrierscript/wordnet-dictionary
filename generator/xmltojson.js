79e29cd6fc3116123f0ff2d6e1fdde8ba854504d
terrierscript <13282103+terrierscript@users.noreply.github.com>
2022-01-08 19:35:11 +0900

    generate

--- generator/xmltojson.js
+++ generator/xmltojson.js
@@ -1,7 +1,7 @@
 const parser = require("fast-xml-parser")
 const fs = require("fs")
 const path = require("path")
-const fileName = "english-wordnet-2020"
+const fileName = "english-wordnet-2021"
 const file = path.join(__dirname, `./source/${fileName}.xml`)
 const data = fs.readFileSync(file).toString()
 
