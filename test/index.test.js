81edb901c9cf15e75c8f6139d2cb7d90728e01f6
terrierscript <13282103+terrierscript@users.noreply.github.com>
2022-01-09 23:03:20 +0900

    fix test

--- test/index.test.js
+++ test/index.test.js
@@ -1,50 +1,68 @@
-
 const assert = require("assert")
-const {  getLexicalEntry, getSynset, getSynsetIndex,getRandomWord } = require("../index")
+const {
+  getLexicalEntry,
+  getSynset,
+  getSynsetIndex,
+  getRandomWord,
+} = require("../dist/index")
 
-const lex = getLexicalEntry("ewn-lie-v")
+const lex = getLexicalEntry("oewn-lie-v")
+console.log(
+  assert.deepStrictEqual(lex.sense, [
+    "oewn-lie__2.42.00..",
+    "oewn-lie__2.35.00..",
+    "oewn-lie__2.42.01..",
+    "oewn-lie__2.42.03..",
+    "oewn-lie__2.32.00..",
+    "oewn-lie__2.42.02..",
+    "oewn-lie__2.38.00..",
+  ]) ?? "✅ searchLexicalEntry sense"
+)
+const synset = getSynset("oewn-02696550-v")
+console.log(
+  assert.deepStrictEqual(synset.definition, [
+    "be located or situated somewhere; occupy a certain position",
+  ]) ?? "✅ synset Definition"
+)
 console.log(
   assert.deepStrictEqual(
-    lex.sense,
+    synset.synsetRelation.map((r) => r.target),
     [
-      'ewn-lie-v-02696550-01',
-      'ewn-lie-v-01549783-01',
-      'ewn-lie-v-02659476-03',
-      'ewn-lie-v-02740213-01',
-      'ewn-lie-v-00835938-01',
-      'ewn-lie-v-02737265-01',
-      'ewn-lie-v-01989043-02'
-    ])
-  ?? "✅ searchLexicalEntry sense"
-)
-const synset = getSynset('ewn-02696550-v')
-console.log(
-  assert.deepStrictEqual(
-    synset.definition, ['be located or situated somewhere; occupy a certain position'],
-  )
-  ?? "✅ synset Definition"
-)
-console.log(
-  assert.deepStrictEqual(
-    synset.synsetRelation.map(r => r.target),
-    [ 'ewn-02737265-v', 'ewn-02661230-v','ewn-02697196-v', 'ewn-02697331-v','ewn-02697564-v', 'ewn-02697732-v','ewn-02697931-v', 'ewn-02698039-v','ewn-02698177-v', 'ewn-02698400-v','ewn-02698528-v', 'ewn-02698930-v','ewn-02699010-v', 'ewn-02699161-v','ewn-02699807-v', 'ewn-02699948-v','ewn-02700129-v', 'ewn-02701971-v','ewn-02709399-v', 'ewn-02719059-v','ewn-02727841-v']
-  )
-  ?? "✅ synset relation"
+      "oewn-02737265-v",
+      "oewn-02661230-v",
+      "oewn-02697196-v",
+      "oewn-02697331-v",
+      "oewn-02697564-v",
+      "oewn-02697732-v",
+      "oewn-02697931-v",
+      "oewn-02698039-v",
+      "oewn-02698177-v",
+      "oewn-02698400-v",
+      "oewn-02698528-v",
+      "oewn-02698930-v",
+      "oewn-02699010-v",
+      "oewn-02699161-v",
+      "oewn-02699807-v",
+      "oewn-02699948-v",
+      "oewn-02700129-v",
+      "oewn-02701971-v",
+      "oewn-02709399-v",
+      "oewn-02719059-v",
+      "oewn-02727841-v",
+    ]
+  ) ?? "✅ synset relation"
 )
 
-const synset2 = getSynsetIndex('ewn-02709399-v')
+const synset2 = getSynsetIndex("oewn-02709399-v")
 console.log(
-  assert.deepStrictEqual(
-    synset2.lexicalEntry, [
-      'ewn-run_along-v',
-      'ewn-line-v'
-    ]
-  )
-  ?? "✅ synset members"
+  assert.deepStrictEqual(synset2.lexicalEntry, [
+    "oewn-line-v",
+    "oewn-run_along-v",
+  ]) ?? "✅ synset members"
 )
 
 // searchSynset()
 for (let i = 0; i < 100; i++) {
   const random = getRandomWord()
-  assert.strictEqual(typeof random ,"string")
+  assert.strictEqual(typeof random, "string")
 }
